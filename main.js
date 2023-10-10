e12246c7ccc52552ee055a91d63becf4a62c8e41
anacarolina124 <137096291+anacarolina124@users.noreply.github.com>
2023-10-10 15:29:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
- function tocaSomPom(){
-document.querySelector('#som_tecla_pom').play('');
+ function tocaSom(idElementoaudio){
+document.querySelector(idElementoaudio).play();
  }
-document.querySelector('.tecla_pom');
-document.querySelectorAll('tecla')
+
+const listadeteclas = document.querySelector('.tecla');
+
+for (contador = 0: contador <listadeteclas.length; contador++){
+
+const instrumento = listadeteclas [contador].classList[1];
+
+
 document.querySelector('#som_tecla_pom').play('');
 
 
+tecla.onclick = function(){
+tocaSo(idaudio);
+};
 
-
- document.querySelector('.tecla_pom').onclick =tocaSomPom();
